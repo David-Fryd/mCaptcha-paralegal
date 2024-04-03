551e2259fd92ac828c2b7aed8a94dad6a0f181f7
David-Fryd <david@fryd.com>
2024-04-03 02:37:23 -0400

    feat: paralegal runs w/ dummy policy

---
+++ utils/paralegal-policies/deletion-policy/src/main.rs
@@ -0,0 +1,39 @@
+use anyhow::Result;
+use paralegal_policy::{
+    assert_error, paralegal_spdg::traverse::EdgeSelection, Context, Marker,
+};
+use std::sync::Arc;
+
+fn dummy_policy(_ctx: Arc<Context>) -> Result<()> {
+    println!("Graph loaded.");
+    Ok(())
+}
+
+fn main() -> Result<()> {
+    let dir = "../../../../mCaptcha-paralegal/";
+    paralegal_policy::SPDGGenCommand::global()
+        .external_annotations("external-annotations.toml")
+        .run(dir)?
+        .with_context(dummy_policy)?;
+    println!("Policy successful");
+    Ok(())
+}
+
+#[allow(dead_code)]
+fn deletion_policy(ctx: Arc<Context>) -> Result<()> {
+    let user_data_types = ctx.marked_type(Marker::new_intern("user_data"));
+
+    let found = ctx.all_controllers().any(|(deleter_id, _ignored)| {
+        let delete_sinks = ctx
+            .all_nodes_for_ctrl(deleter_id)
+            .filter(|n| ctx.has_marker(Marker::new_intern("deletes"), *n))
+            .collect::<Vec<_>>();
+        user_data_types.iter().all(|&t| {
+            let sources = ctx.srcs_with_type(deleter_id, t).collect::<Vec<_>>();
+            ctx.any_flows(&sources, &delete_sinks, EdgeSelection::Data)
+                .is_some()
+        })
+    });
+    assert_error!(ctx, found, "Could not find a controller deleting all types");
+    Ok(())
+}
